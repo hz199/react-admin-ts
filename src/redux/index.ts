071634bf966e6ab43ea450ac5b714307c910ef57
H.Z <15738818183@163.com>
2019-09-12 15:38:49 +0800

    redux 引入页面

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,11 +1,11 @@
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore, compose, applyMiddleware, Store } from 'redux'
 import rootReducer from './reducers'
 import reactThunk from 'redux-thunk'
-import { RootState } from './Types'
+import { RootState, RootAction } from './Types'
 
 // 创建 tore 引入 redux devtools
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store: RootState = createStore(rootReducer, composeEnhancers(applyMiddleware(reactThunk)))
+const store: Store<RootState, RootAction> = createStore(rootReducer, composeEnhancers(applyMiddleware(reactThunk)))
 
 export default store
