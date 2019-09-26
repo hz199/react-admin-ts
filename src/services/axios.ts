19c1f205da1af56425a7a7267e512106df15e7ec
H.Z <15738818183@163.com>
2019-09-26 16:58:33 +0800

    :art: axios 拦截添加 loadingBar

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import LoadingBar from '@/components/LoadingBar'
+
 // let apiBaseURL = 'https://www.easy-mock.com/mock/5d088415bdc26d23199ba01a'
 const apiBaseURL =
   process.env.NODE_ENV === 'development' ? '/' : 'https://www.fastmock.site/mock/6f76b7d369d0c948b43d58ad8a586bd0'
@@ -10,18 +12,22 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   (config) => {
+    LoadingBar.start()
     return config
   },
   (err) => {
+    LoadingBar.error()
     return Promise.reject(err)
   }
 )
 
 instance.interceptors.response.use(
   (response) => {
+    LoadingBar.finish()
     return response.data
   },
   (err) => {
+    LoadingBar.error()
     return Promise.reject(err)
   }
 )
