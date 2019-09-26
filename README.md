d50d21bfbac1fdd7d7c910a9a086efce6e314e28
H.Z <15738818183@163.com>
2019-09-26 18:20:50 +0800

    :memo: 修改 README

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # react-admin-ts
 
+## 前言
+在之前`react`[项目](https://github.com/hz199/__react-admin-zh-redux)上用 `typescript`重构
+
 ## Project setup
 ```
 yarn install
--- src/components/Layout/Menus/menuConfig.ts
+++ src/components/Layout/Menus/menuConfig.ts
@@ -12,11 +12,6 @@ const menus: Array<MenusConfig> = [
     path: '/app'
   },
   {
-    icon: 'desktop',
-    title: '登录测试页',
-    path: '/app/test'
-  },
-  {
     icon: 'table',
     title: '表格',
     SubMenu: [
@@ -24,11 +19,6 @@ const menus: Array<MenusConfig> = [
         icon: 'table',
         path: '/app/tables/table1',
         title: '基础表格'
-      },
-      {
-        icon: 'table',
-        path: '/app/tables/dragTable',
-        title: '拖拽表格'
       }
     ]
   },
@@ -40,32 +30,6 @@ const menus: Array<MenusConfig> = [
         icon: 'loading-3-quarters',
         path: '/app/components/loadingBar',
         title: 'LoadingBar'
-      },
-      {
-        icon: 'drag',
-        path: '/app/components/draggable',
-        title: '拖拽组件'
-      },
-      {
-        icon: 'loading',
-        path: '/app/components/loading',
-        title: 'loading'
-      }
-    ]
-  },
-  {
-    icon: 'desktop',
-    title: 'css模块化',
-    path: '/app/cssModules'
-  },
-  {
-    icon: 'compass',
-    title: 'Echarts',
-    SubMenu: [
-      {
-        icon: 'line-chart',
-        path: '/app/echarts/lineAndBar',
-        title: '折线柱状图'
       }
     ]
   }
