ee487fdb984d688357ffad228535437ebf92e1fd
H.Z <15738818183@163.com>
2019-10-17 22:55:04 +0800

    🌟: 403页面

--- src/Page.tsx
+++ src/Page.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 import App from './App'
 import NoFind from './containers/NoFind'
 import Login from './containers/Login'
+import Page403 from './containers/403'
 
 const Page = () => (
   <Router>
@@ -12,6 +13,7 @@ const Page = () => (
       <Route path="/app" component={App} />
       <Route exact path="/404" component={NoFind} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/403" component={Page403} />
       <Route render={() => <Redirect to="/404" />} />
     </Switch>
   </Router>
