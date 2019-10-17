5b0f03867a58a30baa36a0359a95445f2699f928
H.Z <15738818183@163.com>
2019-10-17 18:44:07 +0800

    feat: 404

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import './index.less'
 
-const Login = () => <div>Login....</div>
+const Login = () => (
+  <div className="login">
+    <iframe className="login__iframe" src="http://www.jq22.com/js/a11.html"></iframe>
+  </div>
+)
 
 export default Login
