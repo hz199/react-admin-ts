20a9bfd1ea0660565dc244dda0ae60d2b9b7f919
H.Z <15738818183@163.com>
2019-10-29 17:47:03 +0800

    deploy dist 文件发不到 github

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 终止一个错误
+set -e
+# 构建
+yarn run build
+
+# 复制CNAME文件
+cp ./CNAME ./dist
+
+# 进入生成的构建文件夹
+cd dist
+
+git init
+git add -A
+git commit -m "初始化项目"
+git remote add origin git@github.com:hz199/reactAdmin.io.git
+git push -f origin master
+
+cd -
